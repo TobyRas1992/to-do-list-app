b51101910905e46f7d90c6d128f7d6f09373443c
TobyRas1992 <tobiasrasmussen@outlook.com>
2020-10-05 14:37:13 +0200

    removed a lot of unecessary comments from JS file

--- js/script.js
+++ js/script.js
@@ -5,30 +5,8 @@ function newItem() {
     if (inputValue === '') {
         alert('You must write something!');
     } else {
-        li.append(inputValue); //appends value to list item
         let list = $('#list'); // How to assign the list to a variable with jQuery
         list.append(li); // appends the list item to the list (one way of doing it with variables)
-        
+        li.append(inputValue); //appends value to list item
     }
 }
-
-
-
-// crosses out item by clicking on it.
-li.on('click', function () {
-    li.addClass('strike');
-})
-
-// use this method maybe to hide elements
-// newElement.hide();
-// show with: newElement.show();
-// there are more cool methods in the guide!! check them out. 
-
-//use this each() method to iterate over li elements and cross them out. 
-// $('input').each(function (i) {
-//     let inputValue = $(this).val();
-//     let inputName = $(this).attr('name');
-//     if(inputValue().length < 1){
-//         console.log('Please fill out the ' + inputName + ' field')
-//     }
-// });
