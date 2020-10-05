1e5bd9ae1105115d050253d0cf338b76db335fa7
TobyRas1992 <tobiasrasmussen@outlook.com>
2020-10-05 15:09:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 function newItem() {
-    let li = $('<li></li>'); // creates variable that stores a list item 
-    let inputValue = $('#input').val(); // stores input value
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
     if (inputValue === '') {
-        alert('You must write something!');
+        alert("You must write something!");
     } else {
-        let list = $('#list'); // How to assign the list to a variable with jQuery
-        list.append(li); // appends the list item to the list (one way of doing it with variables)
-        li.append(inputValue); //appends value to list item
+        $('#list').append(li);
     }
+
+li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+});
+
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on('click', deleteListitem);
+function deleteListitem () {
+    li.addClass('delete');
 }
+
+$('#list').sortable();
+}
+
